ef4a9ca816582e2694f20eabf88fa570f992b907
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-15 09:34:32 +0300

    Product

---
+++ app/Orchid/Screens/Product/ProdcutScreen.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Orchid\Screens\Product;
+
+use Orchid\Screen\Screen;
+
+class ProdcutScreen extends Screen
+{
+    /**
+     * Query data.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * Display header name.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'ProdcutScreen';
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * Views.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [];
+    }
+}
