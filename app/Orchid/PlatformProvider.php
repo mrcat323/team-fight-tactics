871287139a74f3647d42db692762d7561a144b94
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-15 08:58:15 +0300

    Category

--- app/Orchid/PlatformProvider.php
+++ app/Orchid/PlatformProvider.php
@@ -87,16 +87,16 @@ class PlatformProvider extends OrchidServiceProvider
             //         return Dashboard::version();
             //     }, Color::DARK()),
 
-            // Menu::make(__('Users'))
-            //     ->icon('user')
-            //     ->route('platform.systems.users')
-            //     ->permission('platform.systems.users')
-            //     ->title(__('Access rights')),
+             Menu::make(__('Users'))
+                 ->icon('user')
+                 ->route('platform.systems.users')
+                 ->permission('platform.systems.users')
+                 ->title(__('Access rights')),
 
-            // Menu::make(__('Roles'))
-            //     ->icon('lock')
-            //     ->route('platform.systems.roles')
-            //     ->permission('platform.systems.roles'),
+             Menu::make(__('Roles'))
+                 ->icon('lock')
+                 ->route('platform.systems.roles')
+                 ->permission('platform.systems.roles'),
         ];
     }
 
