aaa17582959f8dc8e0aadb1c17a9676ecd4967ff
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-17 15:19:57 +0300

    Seacr and Product API

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $product = Product::all();
+        $product = Product::paginate(8);
         return response()->json($product);
     }
     public function show($id)
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubscribersController;
 use App\Http\Controllers\API\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,16 +16,15 @@ use App\Http\Controllers\API\AuthController;
 |
 */
 
-Route::prefix('auth')->middleware('api')->controller(AuthController::class)->group(function() {
+Route::prefix('auth')->middleware('api')->controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login')->name('login');
     Route::get('/user', 'user');
     Route::post('/logout', 'logout');
-
     Route::post('subscribe', [SubscribersController::class, 'store'])->name('verification.notice');
     Route::get('/email/verify/{hash}', [SubscribersController::class, 'verify'])->name('subscriber.verify');
     Route::get('/product/{id}', [\App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
     Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
-    Route::get('/category/{id}', [\App\Http\Controllers\CategoryController::class, 'show'])->name('category.show');
-    Route::get('/categories', [\App\Http\Controllers\CategoryController::class, 'index'])->name('category.index');
+    Route::get('/products/search', [\App\Http\Controllers\SearchController::class, 'search'])->name('products.search');
+
 });
