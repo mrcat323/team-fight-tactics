6c41bfe287f4dde5a8b52da312952ef673848959
Roma <129154115+wascrashed@users.noreply.github.com>
2023-08-17 15:39:17 +0300

    Fix search

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SubscribersController;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\SearchController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +27,6 @@ Route::prefix('auth')->middleware('api')->controller(AuthController::class)->gro
 });
 Route::post('subscribe', [SubscribersController::class, 'store'])->name('verification.notice');
 Route::get('/email/verify/{hash}', [SubscribersController::class, 'verify'])->name('subscriber.verify');
-Route::get('/product/{id}', [\App\Http\Controllers\ProductController::class, 'show'])->name('product.show');
-Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index'])->name('product.index');
-Route::get('/search', [\App\Http\Controllers\SearchController::class, 'search'])->name('products.search');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+Route::get('/search', [SearchController::class, 'search'])->name('products.search');
