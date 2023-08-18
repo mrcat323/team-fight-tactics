154e91fd72af5ddc2b88f69595fb37a1e3affb35
Mr. CaT <mrcat323@gmail.com>
2023-08-18 15:02:13 +0500

    Change route name

--- routes/platform.php
+++ routes/platform.php
@@ -2,13 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Orchid\Screens\Examples\ExampleCardsScreen;
-use App\Orchid\Screens\Examples\ExampleChartsScreen;
-use App\Orchid\Screens\Examples\ExampleFieldsAdvancedScreen;
-use App\Orchid\Screens\Examples\ExampleFieldsScreen;
-use App\Orchid\Screens\Examples\ExampleLayoutsScreen;
-use App\Orchid\Screens\Examples\ExampleScreen;
-use App\Orchid\Screens\Examples\ExampleTextEditorsScreen;
 use App\Orchid\Screens\PlatformScreen;
 use App\Orchid\Screens\Role\RoleEditScreen;
 use App\Orchid\Screens\Role\RoleListScreen;
@@ -21,7 +14,6 @@ use Tabuna\Breadcrumbs\Trail;
 use App\Orchid\Screens\Product\ProdcutScreen;
 use App\Orchid\Screens\Category\UpdateCategory;
 use App\Orchid\Screens\Category\AddCategory;
-use App\Orchid\Screens\Category\DeleteCategory;
 use App\Orchid\Screens\Product\AddProduct;
 use App\Orchid\Screens\Product\UpdateProduct;
 /*
@@ -59,7 +51,7 @@ Route::screen('product-edit/{product}', UpdateProduct::class)
     ->name('platform.product.edit');
 
 Route::screen('product', ProdcutScreen::class)
-    ->name('platform.product');
+    ->name('platform.products');
 
 // Platform > Profile
 Route::screen('profile', UserProfileScreen::class)
@@ -123,21 +115,3 @@ Route::screen('roles', RoleListScreen::class)
             ->parent('platform.index')
             ->push(__('Roles'), route('platform.systems.roles'));
     });
-
-// Example...
-Route::screen('example', ExampleScreen::class)
-    ->name('platform.example')
-    ->breadcrumbs(function (Trail $trail) {
-        return $trail
-            ->parent('platform.index')
-            ->push('Example screen');
-    });
-
-Route::screen('example-fields', ExampleFieldsScreen::class)->name('platform.example.fields');
-Route::screen('example-layouts', ExampleLayoutsScreen::class)->name('platform.example.layouts');
-Route::screen('example-charts', ExampleChartsScreen::class)->name('platform.example.charts');
-Route::screen('example-editors', ExampleTextEditorsScreen::class)->name('platform.example.editors');
-Route::screen('example-cards', ExampleCardsScreen::class)->name('platform.example.cards');
-Route::screen('example-advanced', ExampleFieldsAdvancedScreen::class)->name('platform.example.advanced');
-
-//Route::screen('idea', 'Idea::class','platform.screens.idea');
