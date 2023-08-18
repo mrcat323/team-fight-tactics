39de993ac25dc73b35f797b57007d50b3cc6227c
Mr. CaT <mrcat323@gmail.com>
2023-08-18 15:04:37 +0500

    Little fixes

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,8 +1,8 @@
 <p>Привет!</p>
 <p>У нас появился новый Продукт</p>
-<a href="{{ route('product.show', $data['product']['id']) }}">{{ route('product.show',$data['product']['id'] ) }}</a>
+<a href="{{ route('product.show', $data['product']['id']) }}">{{ route('product.show',$data['product']['id']) }}</a>
 <h2>{{$data['product']['name']}}</h2>
-<h3>{{$data['product'] ['description']}}</h3>
-<h4>{{$data['product'] ['price']}}</h4>
-<p>Для отказал от рассылки на ваш email, перейдите по следующей ссылке:</p>
+<h3>{{$data['product']['description']}}</h3>
+<h4>{{$data['product']['price']}}</h4>
+<p>Для отказа от рассылки на ваш email, перейдите по следующей ссылке:</p>
 <a href="{{ route('subscriber.unverify', $data['verification_code']) }}">{{ route('subscriber.unverify', $data['verification_code']) }}</a>
